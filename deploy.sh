851a2e129d806233211f854b57f47757ff208a6a
ofekh <halabi1205@gmail.com>
2024-09-29 17:21:11 +0300

    first push

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 sudo cat nginx-config > /etc/nginx/conf.d/default.conf
 sudo systemctl restart nginx.service
-if [ -e /home/ubuntu/nginx-config ]; then
-  rm /home/ubuntu/nginx-config
-fi
+
